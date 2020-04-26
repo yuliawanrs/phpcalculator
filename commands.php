10c128399929a59a05720bc75261ea32321966b2
Yuliawan Rizka S <yuliawan.rizka.s@gmail.com>
2020-04-26 15:42:30 +0700

    Add multiply command

--- commands.php
+++ commands.php
@@ -3,4 +3,5 @@
 return [
     \Jakmall\Recruitment\Calculator\Commands\AddCommand::class,
     \Jakmall\Recruitment\Calculator\Commands\SubtractCommand::class,
+    \Jakmall\Recruitment\Calculator\Commands\MultiplyCommand::class,
 ];
